22f6768ee72c84219afb716746ee51e45e22daa1
Thoe3840 <theofmichell@gmail.com>
2022-11-12 02:27:02 +0000

    run a sort with buttons

--- sorts.py
+++ sorts.py
@@ -3,7 +3,9 @@ from pygame import *
 import pyautogui
 import math
 from random import shuffle
+from time import sleep
 
+from output import display_list
 from insert import insert_sort
 from bubble import bubble_sort
 
@@ -16,11 +18,29 @@ DISP = display.set_mode((WIDTH, HEIGHT))
 display.set_caption('Sorts')
 
 a = [i for i in range(WIDTH)]
-shuffle(a)
-bubble_sort(a, HEIGHT, DISP)
+display_list(a, HEIGHT, DISP)
 
 running = True
 while running:
+    new_sort = False
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+        # change to buttons later
+        elif event.type == KEYDOWN:
+            if event.key == K_b:
+                new_sort = True
+                caption = 'Bubble Sort'
+                sort_type = bubble_sort
+
+            elif event.key == K_i:
+                new_sort = True
+                caption = 'Insert Sort'
+                sort_type = insert_sort
+
+            if new_sort:
+                display.set_caption(caption)
+                shuffle(a)
+                display_list(a, HEIGHT, DISP)
+                sleep(1)
+                sort_type(a, HEIGHT, DISP)
