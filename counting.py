9c2126700c826647ba31fe6bd24445712bafc58b
Theo Laptop <theofmichell@gmail.com>
2023-11-03 15:36:45 +0000

    generalise counting sort

--- counting.py
+++ counting.py
@@ -1,23 +1,30 @@
 from output import display_list
 import pygame
 
-def counting_sort(a, w, h, disp):
-    m = float('-inf')
-    for x in a:
-        if x > m:
-            m = x
+def counting_sort(a, w, h, disp, p = 0):
+    if p:
+        m = 10
+        def f(x):
+            return (x//p) % 10
+    else:
+        m = float('-inf')
+        for x in a:
+            if x > m:
+                m = x
+        def f(x):
+            return x-1
     c = [0] * m
     b = a.copy()
 
     for j in a:
-        c[j-1] += 1
+        c[f(j)] += 1
     c[0] -= 1
     for j in range(1, m):
         c[j] += c[j-1]
     for i in range(len(b)-1, -1, -1):
         x = b[i]
-        a[c[x-1]] = x
-        c[x-1] -= 1
+        a[c[f(x)]] = x
+        c[f(x)] -= 1
         display_list(a, w, h, disp)
 
         for event in pygame.event.get():
--- radix.py
+++ radix.py
@@ -1,4 +1,5 @@
 from output import display_list
+from counting import counting_sort
 import pygame
 
 def radix_sort(a, w, h, disp):
@@ -8,29 +9,7 @@ def radix_sort(a, w, h, disp):
             m = x
     p = 0
     while 10**p < m:
-        if not counting_sort_digit(a, 10**p, w, h, disp):
-            print('quitting')
+        if not counting_sort(a, w, h, disp, 10**p):
             return False
         p += 1
-    return True
-
-def counting_sort_digit(a, p, w, h, disp):
-    c = [0]*10
-    b = a.copy()
-
-    for j in a:
-        c[(j//p) % 10] += 1
-    c[0] -= 1
-    for j in range(1, 10):
-        c[j] += c[j-1]
-    for i in range(len(b)-1, -1, -1):
-        x = b[i]
-        d = (x//p) % 10
-        a[c[d]] = x
-        c[d] -= 1
-        display_list(a, w, h, disp)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
     return True
